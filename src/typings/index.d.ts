5923cd4098f5fb55a8f3ededbe8ac7803c63c750
97lunarmare <matthewgl0@outlook.com>
2021-04-28 18:59:19 +0100

    Remove unused IRoute

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,14 +1,6 @@
 import { HttpVerb, MiddlewareOrder } from './enums';
 import { RequestHandler } from 'express';
 
-export interface IRoute {
-	path?: string,
-	method?: HttpVerb,
-	handler?: RequestHandler,
-	middlewares?: IMiddlewares,
-	parameters?: any[]
-}
-
 export interface IRouteOptions {
 	method: HttpVerb,
 	path?: string
