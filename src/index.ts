a256d1a9c61ccb29094cce648525168e7638702d
97lunarmare <matthewgl0@outlook.com>
2021-04-28 18:59:29 +0100

    Export decorators in main index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as Controller } from './classes/Controller';
 export { route, get, post, put, patch, del } from './decorators/route';
 export { middleware, before, after } from './decorators/middleware';
-export { body } from './decorators/body';
+export { bind, query, params, body } from './decorators/bind';
 export { default as controller } from './decorators/controller';
+export { registerControllers } from './utils/helpers';
